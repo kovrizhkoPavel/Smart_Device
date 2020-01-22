428237860bdf93e97b08687d2cb7e0e9b6407c16
Pavel <pav-kovrizhko@mail.ru>
2020-01-22 05:41:52 +0300

    js localStorage

--- build/js/main.js
+++ build/js/main.js
@@ -1,7 +1,11 @@
 'use strict';
 var buttonCall = document.querySelector('.header__order-call');
 var popup = document.querySelector('.popup');
+var popupForm = popup.querySelector('.popup__form-feedback');
 var popupClose = popup.querySelector('.popup__button-close');
+var userName = popup.querySelector('[name=user-name]');
+var userPhone = popup.querySelector('[name=user-phone]');
+var textMessage = popup.querySelector('textarea');
 var footerInfo = document.querySelector('.footer__container-info');
 var footerNav = footerInfo.querySelector('.footer__nav');
 var footerContacts = footerInfo.querySelector('.footer__container-contacts');
@@ -9,12 +13,39 @@ var buttonNav = footerInfo.querySelector('.footer__button--nav');
 var buttonInfo = footerInfo.querySelector('.footer__button--contacts');
 var inputsTel = document.querySelectorAll('input[type=tel]');
 
+var isStorageSupport = true;
+var storageName = '';
+var storagePhone = '';
+
 footerInfo.classList.remove('nojs');
+
+try {
+  storageName = localStorage.getItem('name');
+  storagePhone = localStorage.getItem('phone');
+} catch (err) {
+  isStorageSupport = false;
+}
 
 if (buttonCall) {
   buttonCall.addEventListener('click', function (evt) {
     evt.preventDefault();
     popup.classList.add('popup--active');
+    if (storageName) {
+      userName.value = storageName;
+      userPhone.focus();
+    } else if (storagePhone) {
+      userPhone.value = storagePhone;
+      textMessage.focus();
+    } else {
+      userName.focus();
+    }
+  });
+
+  popupForm.addEventListener('submit', function () {
+    if (isStorageSupport) {
+      localStorage.setItem('name', userName.value);
+      localStorage.setItem('phone', userPhone.value);
+    }
   });
 
   popupClose.addEventListener('click', function (evt) {
